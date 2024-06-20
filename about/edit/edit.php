edff7aaa4db1d595fda3e86ae18bf90cd5f6bfc5
Ahmad Junaidi <105684336+Ahmadjunaidi101105@users.noreply.github.com>
2024-06-20 19:05:08 +0700

    Add files via upload

---
+++ about/edit/edit.php
@@ -0,0 +1,29 @@
+<?php
+include '../../../config/connect.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $id = $_POST['id_about'];
+    $judul = $_POST['judul'];
+    $konten = $_POST['konten'];
+
+    $sql = "UPDATE about SET judul = '$judul', konten = '$konten' WHERE id_about = '$id'";
+
+    if ($conn->query($sql)) {
+        ?>
+        <script>
+            window.location = "../../page.php?mod=about";
+        </script>
+        <?php
+    } else {
+        echo "Error executing query: " . $conn->error;
+    }
+}
+// if (isset($_POST['Sort'])) {
+//     $sort = $_POST['Sort'];
+//     if ($sort == 'asc') {
+//         $data = bubbleSort_ASC($data);
+//     } elseif ($sort == 'desc') {
+//         $data = bubbleSortDesc($data);
+//     }
+// }
